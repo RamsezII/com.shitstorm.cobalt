652b11e9ffe115033f912cbe9e6b0e368b169208
ram16 <thomass.montaine@gmail.com>
2025-03-29 22:22:44 +0100

    init

---
+++ Terminal/Terminal.cs
@@ -0,0 +1,13 @@
+using _SGUI_;
+
+namespace _COBALT_
+{
+    public class Terminal : SguiWindow
+    {
+        internal InputText input_out, input_realtime, input_in;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+
+    }
+}
