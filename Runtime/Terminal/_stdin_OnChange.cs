67c23e7143cab465c4d2a2282ced60809e05b118
ram16 <thomass.montaine@gmail.com>
2025-04-15 02:48:40 +0200

    meme eve fonctionne

--- Runtime/Terminal/_stdin_OnChange.cs
+++ Runtime/Terminal/_stdin_OnChange.cs
@@ -14,22 +14,21 @@ namespace _COBALT_
             stdin_frame = Time.frameCount;
             flag_stdin.Update(true);
 
-            if (!string.IsNullOrEmpty(text))
-                switch (shell.current_status.state)
-                {
-                    case CMD_STATES.WAIT_FOR_STDIN:
-                        stdin_save = text;
-                        Command.Line line = new(text, SIGNALS.STDIN_CHANGE, this);
-                        shell.PropagateLine(line);
-                        break;
+            switch (shell.current_status.state)
+            {
+                case CMD_STATES.WAIT_FOR_STDIN:
+                    stdin_save = text;
+                    Command.Line line = new(text, SIGNALS.STDIN_CHANGE, this);
+                    shell.PropagateLine(line);
+                    break;
 
-                    case CMD_STATES.BLOCKING:
-                    default:
-                        stdin_save = string.Empty;
-                        if (!string.IsNullOrEmpty(input_stdin.input_field.text))
-                            input_stdin.ResetText();
-                        break;
-                }
+                case CMD_STATES.BLOCKING:
+                default:
+                    stdin_save = string.Empty;
+                    if (!string.IsNullOrEmpty(input_stdin.input_field.text))
+                        input_stdin.ResetText();
+                    break;
+            }
         }
     }
 }
