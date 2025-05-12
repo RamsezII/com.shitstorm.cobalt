6fb114dc1e1eee86f09c9e4bdd93cd5daa3e3132
ram16 <thomass.montaine@gmail.com>
2025-05-12 18:51:35 +0200

    commencé renommage des fichiers textes pour meilleur parsage

--- Runtime/Extras/CmdUI.cs
+++ Runtime/Extras/CmdUI.cs
@@ -1,4 +1,9 @@
-﻿using _COBRA_;
+﻿using _ARK_;
+using _COBRA_;
+using _SGUI_;
+using _UTIL_;
+using System;
+using System.IO;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -27,6 +32,27 @@ namespace _COBALT_
                         exe.Stdout(EventSystem.current.currentSelectedGameObject.transform.GetPath(true));
                 },
                 args: null);
+
+            Command.static_domain.AddAction(
+                "edit-json",
+                min_args: 1,
+                args: static exe =>
+                {
+                    if (exe.line.TryReadArgument(out string file_path, out bool seems_valid, strict: true, search_pattern: "*" + JSon.json, path_mode: FS_TYPES.FILE))
+                        exe.args.Add(file_path);
+                },
+                action: static exe =>
+                {
+                    string file_path = exe.shell.PathCheck((string)exe.args[0], PathModes.ForceFull);
+                    string file_name = Path.GetFileName(file_path);
+                    string type_name = file_name[..^JSon.json.Length];
+                    string assembly_name = file_name[..type_name.IndexOf('.')];
+                    string type_full_name = $"{type_name}, shitstorm.{assembly_name}";
+                    Type type = Type.GetType(type_full_name);
+
+                    SguiCustom window = SguiWindow.InstantiateWindow<SguiCustom>();
+                    window.EditJSon(JsonUtility.FromJson(File.ReadAllText(file_path), type));
+                });
         }
     }
 }
