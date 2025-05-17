8bc9dfc885029ae53bbeef2d5f21d2d4fe8756a7
ram16 <thomass.montaine@gmail.com>
2025-05-17 04:15:41 +0200

    showdialog devient window2

--- Runtime/Extras/_ShowDialog.cs
+++ Runtime/Extras/_ShowDialog.cs
@@ -1,6 +1,5 @@
 ﻿using _COBRA_;
 using _SGUI_;
-using _UTIL_;
 using System.Collections.Generic;
 
 namespace _COBALT_
@@ -11,44 +10,55 @@ namespace _COBALT_
         {
             const string
                 opt_title = "--title",
-                opt_text = "--text",
                 opt_ok_button = "--ok-button",
                 opt_cancel_button = "--cancel-button";
 
             Command.static_domain.AddRoutine(
                 "show-dialog",
+                max_args: 1,
                 opts: static exe =>
                 {
-                    if (exe.line.TryRead_options(exe, out var output, opt_text, opt_title, opt_ok_button, opt_cancel_button))
+                    if (exe.line.TryRead_options(exe, out var output, opt_title, opt_ok_button, opt_cancel_button))
                         foreach (var pair in output)
                             exe.opts.Add(pair.Key, pair.Value);
                     else
                         exe.error = $"'{exe.command.name}' problem reading option ({nameof(exe.line.arg_last)}: '{exe.line.arg_last}')";
+                },
+                args: static exe =>
+                {
+                    if (exe.line.TryReadArgument(out string arg, out _))
+                        exe.args.Add(arg);
                 },
                 routine: EShowDialog
                 );
 
             static IEnumerator<CMD_STATUS> EShowDialog(Command.Executor exe)
             {
-                SguiDialog dialog = SguiDialog.ShowDialog<SguiDialog>(out var routine);
-                dialog.onDestroy += exe.line.shell.terminal.ForceSelectStdin;
+                SguiCustom sgui = SguiWindow.InstantiateWindow<SguiCustom>();
+
+                sgui.onDestroy += exe.line.shell.terminal.ForceSelectStdin;
+                sgui.onAction_confirm += () => exe.Stdout(true);
+                sgui.onAction_cancel += () => exe.Stdout(false);
+
+                var alert = sgui.AddButton<SguiCustom_Alert>();
+
+                alert.SetType(SguiCustom_Alert.DialogTypes.Dialog);
+
+                if (exe.args.Count > 0)
+                    if (exe.opts.TryGetValue((string)exe.args[0], out object text))
+                        alert.SetText(new((string)text));
 
                 if (exe.opts.TryGetValue(opt_title, out object title))
-                    dialog.trad_title.SetTrad((string)title);
-
-                if (exe.opts.TryGetValue(opt_text, out object text))
-                    dialog.SetText(new((string)text));
+                    sgui.trad_title.SetTrad((string)title);
 
                 if (exe.opts.TryGetValue(opt_ok_button, out object ok_button))
-                    dialog.button_yes.GetComponentInChildren<Traductable>().SetTrad((string)ok_button);
+                    sgui.trad_confirm.SetTrad((string)ok_button);
 
                 if (exe.opts.TryGetValue(opt_cancel_button, out object cancel_button))
-                    dialog.button_no.GetComponentInChildren<Traductable>().SetTrad((string)cancel_button);
+                    sgui.trad_cancel.SetTrad((string)cancel_button);
 
-                while (routine.MoveNext())
+                while (sgui != null && !sgui.oblivionized)
                     yield return new CMD_STATUS(CMD_STATES.BLOCKING);
-
-                exe.Stdout(routine.Current);
             }
         }
     }
