c8db5d92af1b1a5e9ef5b2a42c77911917e0faae
ram16 <thomass.montaine@gmail.com>
2025-05-30 18:00:07 +0200

    completion intellisense plus ou moins opérationnelle

--- Runtime/Constrictor/_Linter.cs
+++ Runtime/Constrictor/_Linter.cs
@@ -1,4 +1,5 @@
 ﻿using _COBRA_;
+using _SGUI_;
 using TMPro;
 using UnityEngine;
 
@@ -14,7 +15,7 @@ namespace _COBALT_
         {
             base.OnLint();
 
-            completor.ResetIntellisense();
+            SguiCompletor.instance.ResetIntellisense();
 
             string text = main_input_field.text;
             if (string.IsNullOrWhiteSpace(text))
@@ -31,7 +32,8 @@ namespace _COBALT_
                     lines[i] = terminal.linter.GetLint(terminal.shell, text_line, out Command.Line cmd_line, cursor_i: main_input_field.caretPosition - character_count, flags: SIG_FLAGS.LIST);
                     if (cmd_line.completions != null)
                     {
-                        TMP_CharacterInfo info = main_input_field.textComponent.textInfo.characterInfo[character_count + cmd_line.cpl_last_i];
+                        int text_index = character_count + cmd_line.last_start_i;
+                        TMP_CharacterInfo info = main_input_field.textComponent.textInfo.characterInfo[text_index];
 
                         // Passe en world space
                         Vector3 worldPos = main_input_field.textComponent.rectTransform.TransformPoint(info.bottomLeft);
@@ -39,7 +41,7 @@ namespace _COBALT_
                         // Passe en écran (optionnel, utile pour placer une fenêtre UI dans le canvas Overlay)
                         Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, worldPos);
 
-                        completor.UpdateIntellisense(screenPos, cmd_line.completions);
+                        SguiCompletor.instance.PopulateCompletions(character_count + cmd_line.last_start_i, character_count + cmd_line.last_read_i, screenPos, cmd_line.completions);
                     }
                 }
                 character_count += 1 + text_line.Length;
