191f46af0a762b2855eae0e99b9c39fbe1df63c0
ram16 <thomass.montaine@gmail.com>
2025-06-19 19:19:25 +0200

    commencé ShellView

--- Runtime/ShellView/ShellView.cs
+++ Runtime/ShellView/ShellView.cs
@@ -1,10 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 namespace _COBALT_
 {
     internal sealed partial class ShellView : MonoBehaviour
     {
-        public ShellText std_out, std_prefixe, std_in, std_progress;
+        public ShellText std_out, std_in, std_progress;
+        public TextMeshProUGUI tmp_progress;
 
         //----------------------------------------------------------------------------------------------------------
 
