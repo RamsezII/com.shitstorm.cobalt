efebed1f32339444e1d06f98f0131f0e8f71c7d8
ram16 <thomass.montaine@gmail.com>
2025-06-19 23:47:30 +0200

    vers harbinger

---
+++ Runtime/HarbingerView/HarbingerView.cs
@@ -0,0 +1,29 @@
+﻿using _BOA_;
+using _SGUI_;
+
+namespace _COBALT_
+{
+    public sealed partial class HarbingerView : ShellView
+    {
+        public Shell shell;
+
+        //----------------------------------------------------------------------------------------------------------
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        //----------------------------------------------------------------------------------------------------------
+
+        protected override char OnValidateInput(string text, int charIndex, char addedChar)
+        {
+            return base.OnValidateInput(text, charIndex, addedChar);
+        }
+
+        protected override void OnValueChanged(string value)
+        {
+            base.OnValueChanged(value);
+        }
+    }
+}
