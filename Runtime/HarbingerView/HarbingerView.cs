64778cc3d51c0ca06fab0b133a8d7ab6bc8a5247
ram16 <thomass.montaine@gmail.com>
2025-06-20 02:27:57 +0200

    continuer

--- Runtime/HarbingerView/HarbingerView.cs
+++ Runtime/HarbingerView/HarbingerView.cs
@@ -1,16 +1,21 @@
 ﻿using _BOA_;
 using _SGUI_;
+using System;
+using UnityEngine;
 
 namespace _COBALT_
 {
     public sealed partial class HarbingerView : ShellView
     {
+        public SguiTerminal terminal;
         public Shell shell;
+        [SerializeField] string stdin_save;
 
         //----------------------------------------------------------------------------------------------------------
 
         protected override void Awake()
         {
+            terminal = GetComponentInParent<SguiTerminal>();
             shell = GetComponent<Shell>();
             base.Awake();
         }
@@ -19,12 +24,48 @@ namespace _COBALT_
 
         protected override char OnValidateInput(string text, int charIndex, char addedChar)
         {
+            if (shell.current_status.state != Contract.Status.States.WAIT_FOR_STDIN)
+            {
+                std_in.ResetText();
+                return '\0';
+            }
+
+            try
+            {
+                switch (addedChar)
+                {
+                    case '\t':
+                        OnTab(text, charIndex);
+                        return '\0';
+
+                    case '\n':
+                        OnSubmit(text);
+                        return '\0';
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e, this);
+                return '\0';
+            }
             return addedChar;
         }
 
         protected override void OnValueChanged(string value)
         {
+            switch (shell.current_status.state)
+            {
+                case Contract.Status.States.WAIT_FOR_STDIN:
+                    stdin_save = value;
+                    break;
 
+                case Contract.Status.States.BLOCKING:
+                default:
+                    stdin_save = string.Empty;
+                    if (!string.IsNullOrEmpty(std_in.inputfield.text))
+                        std_in.ResetText();
+                    break;
+            }
         }
     }
 }
---
+++ Runtime/SguiTerminal/SguiTerminal.cs
@@ -0,0 +1,25 @@
+﻿using _BOA_;
+
+namespace _COBALT_
+{
+    public sealed partial class SguiTerminal : _SGUI_.SguiTerminal
+    {
+        public HarbingerView harbinger_view;
+
+        //----------------------------------------------------------------------------------------------------------
+
+        protected override void Awake()
+        {
+            harbinger_view = GetComponentInChildren<HarbingerView>();
+            base.Awake();
+        }
+
+        //----------------------------------------------------------------------------------------------------------
+
+        protected override void Start()
+        {
+            harbinger_view.shell.lint_theme = LintTheme.theme_dark;
+            base.Start();
+        }
+    }
+}
