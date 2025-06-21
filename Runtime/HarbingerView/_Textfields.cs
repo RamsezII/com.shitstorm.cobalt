ed3674771f0cbed224640623e05e4a76598999fc
ram16 <thomass.montaine@gmail.com>
2025-06-22 01:58:11 +0200

    Update _Textfields.cs

--- Runtime/HarbingerView/_Textfields.cs
+++ Runtime/HarbingerView/_Textfields.cs
@@ -38,8 +38,6 @@ namespace _COBALT_
             stdout_field.inputfield.text = shell.stdout_text;
             stdout_field.lint.text = shell.stdout_lint;
 
-            Debug.Log(shell.stdout_lint, this);
-
             stdout_h = 0;
             if (!string.IsNullOrWhiteSpace(shell.stdout_text))
             {
