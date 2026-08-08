d7a25557bf5f3a4c4e4101b24a284b5947287a7f
ram16 <thomass.montaine@gmail.com>
2026-08-08 23:08:54 +0200

    ArkUI

--- Runtime/SguiNotepad/SguiNotepad.cs
+++ Runtime/SguiNotepad/SguiNotepad.cs
@@ -40,7 +40,7 @@ namespace _COBALT_
                     instance = null;
                     return $"can not find file '{file_path}'\n";
                 }
-            instance = Util.InstantiateOrCreate<SguiNotepad>(parent: SguiGlobal.instance.rt_windows2);
+            instance = OSView.InstantiateSoftware<SguiNotepad>();
             instance.Init_file(file_path);
             return null;
         }
